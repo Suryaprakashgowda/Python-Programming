dd84cf1d92ed45fdd8f45849511e1881b5c5e8df
Suryaprakashgowda H S <srinivasagowdabv66@gmail.com>
2026-06-03 09:46:45 +0530

    first commit

---
+++ largest_val.py
@@ -0,0 +1,20 @@
+#wap to find the largest element in the given list.
+
+# def largest_val(l):
+#     l=list(set(l))
+#     l.sort()
+#     return l[-1]
+# l=eval(input("Enter a list: "))
+# print(largest_val(l))
+
+
+
+#wap to remove the duplicates from the given list.
+#
+
+#
+
+
+
+
+#wap to find the second largest element in list?
